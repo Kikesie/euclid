0de0d7186d65817cc2063cbfb6604c2c69d07f11
Anastasia Fabricieva <douughhnutt@gmail.com>
2021-02-05 22:56:18 +0300

    deploy

---
+++ js/preview-slider.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+
+  pagination: {
+    el: ".preview__pagination",
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 2500,
+  },
+
+});
